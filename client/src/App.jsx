b2c4e6424475f2abe6d4d0df8c5080e86a1dc08f
projectblurimedia <projectblurimedia@gmail.com>
2025-09-11 13:47:48 +0530

    Started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./App.css"
+import Login from './pages/login/Login'
+import Home from './pages/home/Home'
 
 function App() {
   return (
-    <div className="schoolExpensesWebsiteContainer">
-      Let's build our First Earning Project
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
