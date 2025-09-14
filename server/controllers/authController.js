7f8429b2758e127900fab2e4b40c2a1b093db97a
projectblurimedia <projectblurimedia@gmail.com>
2025-09-14 10:16:40 +0530

    Expenses And Login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,81 +1,70 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const asyncHandler = require('express-async-handler');
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
 
-// Register user (for creating new users with hashed password)
 const registerUser = asyncHandler(async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
-  // Validate input
   if (!username || !password) {
-    res.status(400);
-    throw new Error('Please provide username and password');
+    res.status(400)
+    throw new Error('Please provide username and password')
   }
 
-  // Check if user already exists
-  const userExists = await User.findOne({ username });
+  const userExists = await User.findOne({ username })
   if (userExists) {
-    res.status(400);
-    throw new Error('Username already exists');
+    res.status(400)
+    throw new Error('Username already exists')
   }
 
-  // Hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
 
-  // Create user
   const user = await User.create({
     username,
     password: hashedPassword,
-  });
+  })
 
-  // Generate JWT
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: '1d',
-  });
+  })
 
   res.status(201).json({
     _id: user._id,
     username: user.username,
     token,
-  });
-});
+  })
+})
 
-// Login user
 const loginUser = asyncHandler(async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
-  // Validate input
   if (!username || !password) {
-    res.status(400);
-    throw new Error('Please provide username and password');
+    res.status(400)
+    throw new Error('Please provide username and password')
   }
 
-  // Find user
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username })
   if (!user) {
-    res.status(401);
-    throw new Error('Invalid username or password');
+    res.status(401)
+    throw new Error('Invalid username or password')
   }
 
-  // Compare password
-  const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, user.password)
   if (!isMatch) {
-    res.status(401);
-    throw new Error('Invalid username or password');
+    res.status(401)
+    throw new Error('Invalid username or password')
   }
 
-  // Generate JWT
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: '1d',
-  });
+  })
 
   res.status(200).json({
     _id: user._id,
     username: user.username,
     token,
-  });
-});
+  })
+})
 
-module.exports = { registerUser, loginUser };
+module.exports = { registerUser, loginUser }
--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -13,6 +13,6 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Password is required'],
     minlength: [6, 'Password must be at least 6 characters long'],
   },
-});
+})
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
